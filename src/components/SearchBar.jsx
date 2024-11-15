393ecbf35f66140e29673954d84156e7dcb481e3
SUNGMIN PARK <ape072399@gmail.com>
2024-11-15 15:41:47 +0900

    검색 구현 완료

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 /**
  * 검색 조건: 영상 제목, 채널 이름, 검색 결과 없으면 없다고 하고 있으면 총 몇건 해서 나오게 하기
- * 
  * 영상 제목, 채널 이름 중 선택할 수 있게 하기
  */
 
-export default function SearchBar(onSearch, searchTerm) {
+export default function SearchBar({onSearch}) {
 
-    // 필터 함수를 이용해 필터링 조건을 검색어를 넣기 
+    const [term, setTerm] = useState("");
 
     // 배열에 검색 분류, 검색어 담기
 
+    // input에서 입력된 검색어 받기
     const handleInputChange = (event) => {
-        const term = event.target.value;
+        setTerm(event.target.value);
+    };
+
+    // 검색어 넘기기
+    const handleSearch = () => {
         onSearch(term);
-    }
-    
-    return(
+    };
 
+    return (
         <div>
-            <input placeholder="검색어를 넣으세요" value={searchTerm}/>
-            <button onClick={handleInputChange}>검색</button>
+            {/* 드롭박스 */}
+            <input
+                type="text"
+                placeholder="검색어를 넣으세요"
+                value={term}
+                onChange={handleInputChange}
+            />
+            <button onClick={handleSearch}>검색</button>
         </div>
-    )
-
+    );
 }
--- src/pages/ListModal.jsx
+++ src/pages/ListModal.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axiosInstance from "../api/youtubeAxios";
 import styles from '../css/ListModal.module.css'; // css 폴더 내의 CSS 모듈 import
 import { useNavigate } from "react-router-dom"; // 페이지 이동을 위해 react-router-dom 사용
 import CustomPagination from "../components/CustomPagination";
 import SearchBar from "../components/SearchBar";
 
-// TODO 검색 만들기
 function ListModal({ onUserClick, closeModal }) {
     const [datas, setDatas] = useState([]);
     const [showModal, setShowModal] = useState(false);
@@ -13,10 +12,7 @@ function ListModal({ onUserClick, closeModal }) {
     const [currentPage, setCurrentPage] = useState(1);
     const [searchTerm, setSearchTerm] = useState("");
 
-    // const smapleData = ['title', 'channelTitle'];
-
     const itemsPerPage = 4;
-
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -26,7 +22,7 @@ function ListModal({ onUserClick, closeModal }) {
     const fetchDatas = async (page) => {
         try {
             const response = await axiosInstance.get('/db', { params: { page } });
-            setDatas(prevDatas => [...prevDatas, ...response.data.db[0].items || []]);
+            setDatas(response.data.db[0].items || []);
         } catch (error) {
             console.error("Error fetching users:", error);
         }
@@ -53,14 +49,13 @@ function ListModal({ onUserClick, closeModal }) {
         setCurrentPage(pageNumber);
     };
 
-    // 검색어를 업데이트 해주는 함수
     const onSearch = (term) => {
-        setSearchTerm(term)
-    }
+        setSearchTerm(term);
+    };
 
     const filteredData = datas.filter((data) =>
-        data.snippet.title.toLowerCase().includes(searchTerm.toLowerCase())
-        // data.snippet.channelTitle.toLowerCase().includes(searchTerm.toLowerCase())
+        data.snippet.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        data.snippet.channelTitle.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     const indexOfLastItem = currentPage * itemsPerPage;
@@ -72,8 +67,7 @@ function ListModal({ onUserClick, closeModal }) {
             <button className={styles.closeButton} onClick={closeModal}>닫기</button>
             <h2 className={styles.title}>Video List Modal</h2>
 
-            {/* <Dropdown props={smapleData} /> */}
-            <SearchBar searchTerm={searchTerm} onSearch={onSearch} />
+            <SearchBar onSearch={onSearch} />
 
             <ul className={styles.list}>
                 {currentItems.length > 0 ? (
@@ -97,7 +91,7 @@ function ListModal({ onUserClick, closeModal }) {
                 )}
             </ul>
             <CustomPagination
-                totalItemsCount={datas.length}
+                totalItemsCount={filteredData.length}
                 itemsCountPerPage={itemsPerPage}
                 onPageChange={handlePageChange}
             />
