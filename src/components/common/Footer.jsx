46b97b4cc57ff298483c3235b69db1dad7bd658c
SUNGMIN PARK <ape072399@gmail.com>
2024-11-22 09:40:59 +0900

    헤더에만 번역 적용

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,22 +1,21 @@
 import classes from '@css/Header.module.css';
-import { useTranslation } from 'react-i18next';
+
 import { MdPostAdd, MdMessage } from 'react-icons/md';
 
 export default function Footer() {
 
-  const  {t} = useTranslation();
+  
 
     return (
         <header className={classes.header}>
       <h1 className={classes.logo}
-      mainTitle={t(`footer.maintitle`)}
+    
       >
         <MdMessage />
         푸터
       </h1>
       <p>
         <button className={classes.button}
-         mainTitle={t(`footer.subTitle`)}
         >
           <MdPostAdd size={18} />
           푸터
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,18 +2,17 @@ import { useNavigate } from 'react-router-dom';
 import classes from '@css/Header.module.css';
 import { MdPostAdd, MdMessage } from 'react-icons/md';
 import { TfiWorld } from "react-icons/tfi";
-import i18n from "../../lang/i18n"
+import i18n from "../../lang/i18n";
 import { useTranslation } from 'react-i18next';
 import { useRecoilState } from 'recoil';
 import { LangState } from '../../recoil/LangAtom';
-
 
 export default function Header() {
 
   const [lang, setLang] = useRecoilState(LangState);
   const isEnglish = lang === "eng";
 
-  const  {t} = useTranslation();
+  const { t } = useTranslation();
 
   const navigate = useNavigate();
 
@@ -30,51 +29,37 @@ export default function Header() {
   };
 
   const handleLang = () => {
-
     const newLang = isEnglish ? "kr" : "eng";
     setLang(newLang);
     i18n.changeLanguage(newLang);
     console.log("newLang: " + newLang);
-    
     localStorage.setItem("language", newLang);
-
-    console.log("leng: " +lang);
-    
-    
-  }
+    console.log("lang: " + lang);
+  };
 
   return (
     <header className={classes.header}>
-      <h1 className={classes.logo} onClick={handleMain}
-        title={t(`header.title`)}
-      >
+      <h1 className={classes.logo} onClick={handleMain} title={t('header.title')}>
         <MdMessage />
         한국근대교육의 주체, 공간, 경험
       </h1>
       <div className={classes.navLinks}>
-        <p onClick={goKyoWonDea}
-        homepage={t(`header.homepage`)}
-        
-        >한국교원대 홈피</p>
-        <p onClick={handleList}
-        list={t(`header.list`)}
-        >리스트</p>
-        <p
-        institute={t(`header.institute`)}
-        >한국근대교육사 연구센터</p>
+        <p onClick={goKyoWonDea} data-homepage={t('header.homepage')}>
+          한국교원대 홈피
+        </p>
+        <p onClick={handleList} data-list={t('header.list')}>
+          리스트
+        </p>
+        <p data-institute={t('header.institute')}>
+          한국근대교육사 연구센터
+        </p>
       </div>
       {/* 한글, 영어로 변경 */}
-
-      <button className={classes.button} onClick={handleLang}
-      langChange={t(`header.langChange`)}
-      >
+      <button className={classes.button} onClick={handleLang} data-langchange={t('header.langChange')}>
         <TfiWorld size={18} />
         언어 변경
       </button>
-
-      <button className={classes.button}
-      header={t(`header.header`)}
-      >
+      <button className={classes.button} data-header={t('header.header')}>
         <MdPostAdd size={18} />
         해더
       </button>
